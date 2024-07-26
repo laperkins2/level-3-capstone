5b353702550ec43306fcff171ccf8cc202d84f96
laperkins2 <67718788+laperkins2@users.noreply.github.com>
2024-07-26 12:19:11 -0500

    Reverting to 4cf1eccfdbbafbae7098e229ee1321bccf7284d2

---
+++ __mocks__/firebase.config.js
@@ -0,0 +1,5 @@
+export const auth = {
+  currentUser: {
+    /* mock user object */
+  },
+};
---
+++ src/utils/mockFirebase.utils.js
@@ -0,0 +1,4 @@
+export const getAllDocuments = jest.fn();
+export const addDocument = jest.fn();
+export const updateDocument = jest.fn();
+export const deleteDocument = jest.fn();

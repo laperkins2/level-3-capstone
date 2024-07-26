5e3db942cf0050390eedf293e411318772417578
laperkins2 <67718788+laperkins2@users.noreply.github.com>
2024-07-26 14:22:54 -0500

    deleted testing mock files

--- __mocks__/firebase.config.js
+++
@@ -1,5 +0,0 @@
-export const auth = {
-  currentUser: {
-    /* mock user object */
-  },
-};
--- src/utils/mockFirebase.utils.js
+++
@@ -1,4 +0,0 @@
-export const getAllDocuments = jest.fn();
-export const addDocument = jest.fn();
-export const updateDocument = jest.fn();
-export const deleteDocument = jest.fn();
